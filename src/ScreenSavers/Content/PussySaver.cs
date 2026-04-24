6d27de8d13284d07ceeb93f94508106bf2096a9f
Darkmango <DarkmangoTheMango@gmail.com>
2026-04-23 17:56:02 -0700

    update PussySaver.cs

--- src/ScreenSavers/Content/PussySaver.cs
+++ src/ScreenSavers/Content/PussySaver.cs
@@ -29,10 +29,10 @@ public class PussySaver : ILoadable
 
     public void Load(Mod mod)
     {
-        Cats.Add(new Cat(Main.rand.NextVector2FromRectangle(new(0, 0, Main.screenWidth, Main.screenHeight)), Main.rand.NextVector2Circular(1, 1) * Main.rand.NextFloat(5, 6), "ScreenSavers/Assets/Images/Cat_6"));
+        Cats.Add(new Cat(Main.rand.NextVector2FromRectangle(new(0, 0, Main.screenWidth, Main.screenHeight)), Main.rand.NextVector2CircularEdge(1, 1) * 6, "ScreenSavers/Assets/Images/Cat_6"));
 
-        for (int k = 0; k < 100; k++)
-            Cats.Add(new Cat(Main.rand.NextVector2FromRectangle(new(0, 0, Main.screenWidth, Main.screenHeight)), Main.rand.NextVector2Circular(1, 1) * Main.rand.NextFloat(5, 6), $"ScreenSavers/Assets/Images/Cat_{Main.rand.Next(6)}"));
+        for (int k = 0; k < 299; k++)
+            Cats.Add(new Cat(Main.rand.NextVector2FromRectangle(new(0, 0, Main.screenWidth, Main.screenHeight)), Main.rand.NextVector2CircularEdge(1, 1) * 6, $"ScreenSavers/Assets/Images/Cat_{Main.rand.Next(6)}"));
     }
 
 	public void Unload()
@@ -42,30 +42,49 @@ public class PussySaver : ILoadable
 
 	public static void Draw(SpriteBatch spriteBatch)
     {
-        if (AFK.EffectProgress > 0)
+        if (AFK.EffectProgress < 0)
+            return;
+
+        spriteBatch.Draw(TextureAssets.MagicPixel.Value, new Rectangle(0, 0, Main.screenWidth, Main.screenHeight), Color.Black * AFK.EffectProgress);
+
+        for (int k = 0; k < Cats.Count; k++)
         {
-            spriteBatch.Draw(TextureAssets.MagicPixel.Value, new Rectangle(0, 0, Main.screenWidth, Main.screenHeight), Color.Black * AFK.EffectProgress);
+            var c = Cats[k];
+            Texture2D texture = ModContent.Request<Texture2D>(c.Tex).Value;
 
-            for (int k = 0; k < Cats.Count; k++)
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Matrix.Identity);
+
+            spriteBatch.Draw(texture, c.Pos, texture.Bounds, Color.White * AFK.EffectProgress, 0, texture.Size() * 0.5f, 2, c.Vel.X > 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0f);
+
+            spriteBatch.End();
+            spriteBatch.Begin();
+
+            c.Pos += c.Vel;
+
+            float pad = 50f;
+            Vector2 vel = Main.rand.NextVector2CircularEdge(1, 1) * 6;
+
+            if (c.Pos.X > Main.screenWidth + pad)
             {
-                var c = Cats[k];
-                Texture2D texture = ModContent.Request<Texture2D>(c.Tex).Value;
+                c.Pos.X = -pad;
+                c.Vel = vel;
+            }
+            else if (c.Pos.X < -pad)
+            {
+                c.Pos.X = Main.screenWidth + pad;
+                c.Vel = vel;
+            }
 
-                spriteBatch.Draw(texture, c.Pos, texture.Bounds, Color.White * AFK.EffectProgress, 0, texture.Size() * 0.5f, 2, SpriteEffects.None, 0f);
-
-                c.Pos += c.Vel;
-
-                float pad = 50f;
-
-                if (c.Pos.X > Main.screenWidth + pad)
-                    c.Pos.X = -pad;
-                else if (c.Pos.X < -pad)
-                    c.Pos.X = Main.screenWidth + pad;
-
-                if (c.Pos.Y > Main.screenHeight + pad)
-                    c.Pos.Y = -pad;
-                else if (c.Pos.Y < -pad)
-                    c.Pos.Y = Main.screenHeight + pad;
+            if (c.Pos.Y > Main.screenHeight + pad)
+            {
+                c.Pos.Y = -pad;
+                c.Vel = vel;
+            }
+            else if (c.Pos.Y < -pad)
+            {
+                c.Pos.Y = Main.screenHeight + pad;
+                c.Vel = vel;
             }
         }
     }
